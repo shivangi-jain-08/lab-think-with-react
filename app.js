c43d857d2b5a751b2128fbfab7f09a2598afaf11
shivangi-jain-08 <shivangi.jain@kalvium.community>
2023-12-05 11:34:42 +0530

    done

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
+//React 2 | Check n Check
+// const container = document.getElementById('react-container');
+// ReactDOM.render("Hello! Welcome to React",container);
 
+//------------------------------------------------------------------------
+
+//React 3 | Is It a Function
+// const Container = () => {
+//   return React.createElement(
+//     `div`,
+//     null,
+//     `Hey Kalvians! Welcome to React Learning`,
+//     React.createElement(`div`, null, `Let's rock and roll`)
+//   );
+// };
+// const container = document.getElementById("react-container");
+// ReactDOM.render(React.createElement(Container), container);
+
+//------------------------------------------------------------------------
+
+//REACT 4 | NOW IT'S CLASS TIME
 class ReactContainer extends React.Component {
   render() {
     return React.createElement(
